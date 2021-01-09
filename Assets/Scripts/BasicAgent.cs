2bfc611261f5539dabd911106c45bd0f7f7802fe
neumont-gamedev <rmaple@neumont.edu>
2021-01-09 11:50:38 -0700

    Initial commit

---
+++ Assets/Scripts/BasicAgent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAgent : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float perceptionDistance = 10.0f;
+    public float perceptionAngle = 30.0f;
+    public bool flee = false;
+
+    void Update()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+        GameObject nearestGameObject = GetNearestGameObject(gameObjects, out float nearestDistance);
+
+        if (nearestDistance < perceptionDistance)
+		{
+            Debug.DrawLine(transform.position, nearestGameObject.transform.position);
+            Debug.DrawRay(transform.position, transform.forward, Color.red);
+
+            Vector3 direction = (nearestGameObject.transform.position - transform.position).normalized;
+            float dot = Vector3.Dot(transform.forward, direction);
+            dot = Mathf.Clamp(dot, -1, 1);
+            float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
+
+            if (angle <= perceptionAngle)
+			{
+                direction = (flee) ? -direction : direction;
+                Vector3 velocity = direction * speed;
+                transform.position += velocity * Time.deltaTime;
+
+                if (direction.magnitude > 0.1f)
+                {
+                    transform.rotation = Quaternion.LookRotation(direction);
+                }
+            }
+        }
+
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+    }
+
+    GameObject GetNearestGameObject(GameObject[] gameObjects, out float nearestDistance)
+	{
+        nearestDistance = float.MaxValue;
+
+        GameObject nearestGameObject = null;
+        foreach(GameObject gameObject in gameObjects)
+		{
+            if (gameObject == this.gameObject) continue;
+
+            float distance = Vector3.Distance(transform.position, gameObject.transform.position);
+            if (distance < nearestDistance)
+			{
+                nearestDistance = distance;
+                nearestGameObject = gameObject;
+			}
+		}
+
+        return nearestGameObject;
+	}
+
+
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static Vector3 Wrap(Vector3 v, Vector3 min, Vector3 max)
+    {
+        Vector3 result = v;
+
+        if (result.x > max.x) result.x = min.x;
+        else if (result.x < min.x) result.x = max.x;
+        if (result.y > max.y) result.y = min.y;
+        else if (result.y < min.y) result.y = max.y;
+        if (result.z > max.z) result.z = min.z;
+        else if (result.z < min.z) result.z = max.z;
+
+        // test result.x > max.x, if so result.x = min.x
+        // test result.x < min.x, if so result.x = max.x
+
+        return result;
+    }
+}
