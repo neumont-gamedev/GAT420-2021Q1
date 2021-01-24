791127cec9f36433fa3552b1bd316eb0b1fb1cdf
neumont-gamedev <rmaple@neumont.edu>
2021-01-24 11:52:31 -0700

    class 012221 merged fixed

--- Assets/Scripts/GraphNodeCreator.cs
+++ Assets/Scripts/GraphNodeCreator.cs
@@ -6,16 +6,12 @@ public class GraphNodeCreator : MonoBehaviour
 {
     public GraphNode graphNode;
     public LayerMask layerMask;
-<<<<<<< HEAD
     public float range = 1;
-=======
-	[Range(1, 20)] public float range = 1;
->>>>>>> dda073ab0288a198794f66ff03e2ad04599e1d36
+
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-<<<<<<< HEAD
 		{
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
@@ -39,36 +35,11 @@ public class GraphNodeCreator : MonoBehaviour
                 edge.nodeB = otherNode;
 
                 node.Edges.Add(edge);
-=======
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
-            {
-				GraphNode node = Instantiate(graphNode, hitInfo.point, Quaternion.identity, transform);
-				LinkNode((GraphNode)node, range);
-            }
-        }
-    }
-
-	public void LinkNode(GraphNode node, float range)
-	{
-		Collider[] colliders = Physics.OverlapSphere(node.transform.position, range);
-		foreach (Collider collider in colliders)
-		{
-			GraphNode otherNode = collider.GetComponent<GraphNode>();
-			if (otherNode != null && otherNode != node)
-			{
-				GraphNode.Edge edge;
-				edge.nodeA = node;
-				edge.nodeB = otherNode;
-
-				node.Edges.Add(edge);
->>>>>>> dda073ab0288a198794f66ff03e2ad04599e1d36
 			}
 		}
 	}
 
-<<<<<<< HEAD
+
     public void LinkNodes()
 	{
         GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Node");
@@ -78,21 +49,6 @@ public class GraphNodeCreator : MonoBehaviour
             if (graphNode != null)
 			{
                 LinkNode(graphNode);
-			}
-		}
-	}
-=======
-	public void LinkNodes(float range)
-	{
-		// get all nodes in scene
-		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Node");
-		foreach (GameObject gameObject in gameObjects)
-		{
-			GraphNode node = gameObject.GetComponent<GraphNode>();
-			if (node != null)
-			{
-				// connect neighbor nodes within range
-				LinkNode(node, range);
 			}
 		}
 	}
@@ -106,5 +62,4 @@ public class GraphNodeCreator : MonoBehaviour
 			Destroy(node.gameObject);
 		}
 	}
->>>>>>> dda073ab0288a198794f66ff03e2ad04599e1d36
 }
